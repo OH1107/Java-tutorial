8ed22ab0b025c51a310b9a1e3dcc393390f4b536
오승철 <tmdcjf103@gmail.com>
2021-09-02 02:53:01 +0900

    Initial commit

---
+++ Chapter2/src/chapter2/VariableEx.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class VariableEx {
+	public static void main(String[] args) {
+		
+		int num;
+		num = 10;
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		int numberOfStudent;	// 변수 이름은 의미를 내포하도록
+		int ns;
+		
+		
+	}
+}
