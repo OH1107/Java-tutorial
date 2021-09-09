8bbc78ae03305912ff92eb4ae7b656b6f5f262de
오승철 <tmdcjf103@gmail.com>
2021-09-10 02:19:32 +0900

    Add 클래스와 객체1(3)

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -7,6 +7,15 @@ public class Student {
 	int grade;
 	String address;
 	
+	// 디폴트 생성자
+	public Student() {}
+	
+	// 생성자 오버로드
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+	}
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + ", " + address);
 	}
@@ -18,4 +27,20 @@ public class Student {
 	public void setStudentName(String name) {
 		studentName = name;
 	}
+	
+//	public static void main(String[] args) {
+//		
+//		Student studentLee = new Student();
+//		studentLee.studentName = "이순신";
+//		studentLee.studentID = 100;
+//		studentLee.address = "서울시 영등포구 여의도동";
+//
+//		Student studentKim = new Student();
+//		studentKim.studentName = "김유신";
+//		studentKim.studentID = 101;
+//		studentKim.address = "미국 산호세";
+//		
+//		studentLee.showStudentInfo();
+//		studentKim.showStudentInfo();
+//	}
 }
