e7c7228c28cf40dd3824cb68605e1018bceb8ce6
오승철 <tmdcjf103@gmail.com>
2021-09-20 21:52:10 +0900

    Add 클래스와 객체2(2)

---
+++ Chapter6/src/cooperation/TakeTrans.java
@@ -0,0 +1,23 @@
+package cooperation;
+
+public class TakeTrans {
+
+	public static void main(String[] args) {
+
+		Student james = new Student("James", 5000);
+		Student tomas = new Student("Tomas", 10000);
+		
+		Bus bus100 = new Bus(100);
+		Subway subwayGreen = new Subway(2);
+		
+		james.takeBus(bus100);
+		tomas.takeSubway(subwayGreen);
+		
+		james.showInfo();
+		bus100.showInfo();
+		
+		tomas.showInfo();
+		subwayGreen.showInfo();
+	}
+
+}
